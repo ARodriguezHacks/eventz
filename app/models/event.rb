84af0b9d2c92f6fe7be6fece9c5d02a8665c5945
Angie Rodriguez <angrodri902@gmail.com>
2021-02-01 16:02:01 -0800

    Modify event index action with new query

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ApplicationRecord
+  # Class methods because they start with self
+  def self.upcoming
+    where("starts_at > ?", Time.now).order("starts_at")
+  end
+
   def free?
     price.blank? || price.zero?
   end
